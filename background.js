2c3a0a2aa77bb587038280c28db583cd3e75f0ca
greatghoul <greatghoul@gmail.com>
2016-10-16 15:44:29 +0800

    Implements Quick Access from omnibox

--- background.js
+++ background.js
@@ -1,8 +1,46 @@
+const BASE_URL = 'http://minecraft-zh.gamepedia.com/api.php';
+
+function isBlank(text) {
+  return /^\s*$/.test(text);
+}
+
+function suggestUrl(text) {
+  let keyword = encodeURIComponent(text);
+  return `${BASE_URL}?action=opensearch&format=json&formatversion=2&search=${keyword}&namespace=0&limit=10&suggest=true`
+}
+
+function navigate(url) {
+  chrome.tabs.getSelected(null, function(tab) {
+    chrome.tabs.create({ url: url, active: true, index: tab.index });
+    chrome.tabs.remove(tab.id);
+  });
+}
+
 function inputChangedHandler(text, suggest) {
-  suggest([
-    {content: text + " one", description: "the first one"},
-    {content: text + " number two", description: "the second entry"}
-  ]);
+  if (isBlank(text)) return;
+
+  fetch(suggestUrl(text), { mode: 'cors' })
+    .then(response => response.json())
+    .then(result => {
+      let suggestions = [];
+      
+      for (let i = 0; i < result[1].length; i++) {
+        let title = result[1][i];
+        let link = result[3][i];
+
+        suggestions.push({
+          content: link,
+          description: `<match>${title}</match><dim> - </dim><url>${link}</url>`
+        });
+      }
+
+      suggest(suggestions);
+    });
+}
+
+function inputEnteredHandler(link) {
+  navigate(link);
 }
 
 chrome.omnibox.onInputChanged.addListener(inputChangedHandler);
+chrome.omnibox.onInputEntered.addListener(inputEnteredHandler);
